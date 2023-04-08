4a14cf8c04370285a4a62fe9fc06f3fe74a6c86a
Anupam Maurya <anupammaurya6767@gmail.com>
2023-04-08 08:52:19 +0530

    Time: 79 ms (59.61%), Space: 27.6 MB (80.49%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.cpp
+++ 1020-number-of-enclaves/1020-number-of-enclaves.cpp
@@ -1,19 +1,54 @@
 class Solution {
 public:
-    int numEnclaves(vector<vector<int>>& A, int res = 0) {
-  queue<pair<int, int>> q;
-  for (auto i = 0; i < A.size(); ++i)
-    for (auto j = 0; j < A[0].size(); ++j) {
-      res += A[i][j];
-      if (i * j == 0 || i == A.size() - 1 || j == A[i].size() - 1) q.push({ i, j });
+    int numEnclaves(vector<vector<int>>& grid) {
+        queue<pair<int,int>> q; 
+        int n = grid.size(); 
+        int m = grid[0].size(); 
+        vector<vector<int>>vis(n,vector<int>(m,0)); 
+        // traverse boundary elements
+        for(int i = 0;i<n;i++) {
+            for(int j = 0;j<m;j++) {
+                // first row, first col, last row, last col 
+                if(i == 0 || j == 0 || i == n-1 || j == m-1) {
+                    // if it is a land then store it in queue
+                    if(grid[i][j] == 1) {
+                        q.push({i, j}); 
+                        vis[i][j] = 1; 
+                    }
+                }
+            }
+        }
+        
+        int delrow[] = {-1, 0, +1, 0};
+        int delcol[] = {0, +1, +0, -1}; 
+        
+        while(!q.empty()) {
+            int row = q.front().first; 
+            int col = q.front().second; 
+            q.pop(); 
+            
+            // traverses all 4 directions
+            for(int i = 0;i<4;i++) {
+                int nrow = row + delrow[i];
+                int ncol = col + delcol[i]; 
+                // check for valid coordinates and for land cell
+                if(nrow >=0 && nrow <n && ncol >=0 && ncol < m 
+                && vis[nrow][ncol] == 0 && grid[nrow][ncol] == 1) {
+                    q.push({nrow, ncol});
+                    vis[nrow][ncol] = 1; 
+                }
+            }
+            
+        }
+        
+        int cnt = 0;
+        for(int i = 0;i<n;i++) {
+            for(int j = 0;j<m;j++) {
+                // check for unvisited land cell
+                if(grid[i][j] == 1 & vis[i][j] == 0) 
+                    cnt++; 
+            }
+        }
+        return cnt; 
     }
-  while (!q.empty()) {
-    auto x = q.front().first, y = q.front().second; q.pop();
-    if (x < 0 || y < 0 || x == A.size() || y == A[x].size() || A[x][y] != 1) continue;
-    A[x][y] = 0;
-    --res;
-    q.push({ x + 1, y }), q.push({ x - 1, y }), q.push({ x, y + 1 }), q.push({ x, y - 1 });
-  }
-  return res;
-}
 };
