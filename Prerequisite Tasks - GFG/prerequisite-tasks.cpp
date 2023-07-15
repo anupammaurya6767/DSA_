0e60e5e359fc4e30e5d83b225bf2af4bb116afc5
Anupam Maurya <anupammaurya6767@gmail.com>
2023-07-15 20:09:46 +0530

    Added solution - LeetHub

---
+++ Prerequisite Tasks - GFG/prerequisite-tasks.cpp
@@ -0,0 +1,68 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+public:
+	bool isPossible(int N,int P, vector<pair<int, int> >& pre) {
+	    // Code here
+	    vector<int>adj[N];
+	    vector<int>ans;
+	    vector<int>indegree(N,0);
+	    for(int i=0;i<P;i++)
+	    {
+	        int s = pre[i].second;
+	        int f = pre[i].first;
+	        adj[f].push_back(s);
+	        indegree[s]++;
+	    }
+	    
+	    queue<int>q;
+	    for(int i=0;i<N;i++)
+	    if(indegree[i]==0)
+	    q.push(i);
+	    int curr = 0;
+	    while(!q.empty())
+	    {
+	        int node = q.front();
+	        ans.push_back(node);
+	        q.pop();
+	        for(auto it:adj[node])
+	        {
+	            indegree[it]--;
+	            if(indegree[it]==0)
+	            q.push(it);
+	        }
+	    }
+	    
+	    return ans.size()==N;
+	}
+};
+
+//{ Driver Code Starts.
+int main(){
+	int tc;
+	cin >> tc;
+	while(tc--){
+    	int N, P;
+        vector<pair<int, int> > prerequisites;
+        cin >> N;
+        cin >> P;
+        for (int i = 0; i < P; ++i) {
+            int x, y;
+            cin >> x >> y;
+            prerequisites.push_back(make_pair(x, y));
+        }
+        // string s;
+        // cin>>s;
+        Solution ob;
+        if (ob.isPossible(N,P, prerequisites))
+            cout << "Yes";
+        else
+            cout << "No";
+        cout << endl;
+	}
+	return 0;
+}
+// } Driver Code Ends
