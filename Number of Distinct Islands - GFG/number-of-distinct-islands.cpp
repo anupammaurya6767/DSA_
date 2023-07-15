980efe054cd33535531740cca1c79881c97f008a
Anupam Maurya <anupammaurya6767@gmail.com>
2023-07-15 15:29:54 +0530

    Added solution - LeetHub

--- Number of Distinct Islands - GFG/number-of-distinct-islands.cpp
+++ Number of Distinct Islands - GFG/number-of-distinct-islands.cpp
@@ -9,55 +9,44 @@ using namespace std;
 
 class Solution {
   public:
-    void dfs(int row, int col, vector < vector < int >> & vis,
-      vector < vector < int >> & grid, vector < pair < int, int >> & vec, int row0, 
-      int col0) {
-      // mark the cell as visited
-      vis[row][col] = 1;
-
-      // coordinates - base coordinates
-      vec.push_back({
-        row - row0,
-        col - col0
-      });
-      int n = grid.size();
-      int m = grid[0].size();
-
-      // delta row and delta column
-      int delrow[] = {-1, 0, +1, 0}; 
-      int delcol[] = {0, -1, 0, +1}; 
-
-      // traverse all 4 neighbours
-      for (int i = 0; i < 4; i++) {
-        int nrow = row + delrow[i];
-        int ncol = col + delcol[i];
-        // check for valid unvisited land neighbour coordinates 
-        if (nrow >= 0 && nrow < n && ncol >= 0 && ncol < m &&
-          !vis[nrow][ncol] && grid[nrow][ncol] == 1) {
-          dfs(nrow, ncol, vis, grid, vec, row0, col0);
+   void bfs(int row, int col,vector<vector<int>>&vis,vector<pair<int,int>>&vec,vector<vector<int>>&grid,
+   int row0, int col0,int n, int m)
+   {
+       vis[row][col] = 1;
+       vec.push_back({row-row0,col-col0});
+       int di[] = {-1,0,1,0};
+       int dj[] = {0,1,0,-1};
+       
+       for(int i=0;i<4;i++)
+       {
+           int nr = row+di[i];
+           int nc = col + dj[i];
+           if(nr>=0 && nc>=0 && nr<n && nc<m && !vis[nr][nc] && grid[nr][nc]==1)
+         {
+             bfs(nr,nc,vis,vec,grid,row0,col0,n,m);
+         }
+       }
+   }
+    int countDistinctIslands(vector<vector<int>>& grid) {
+        // code here
+        int n = grid.size();
+        int m= grid[0].size();
+        vector<vector<int>>vis(n,vector<int>(m,0));
+        set<vector<pair<int,int>>>s;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                if(!vis[i][j]&&grid[i][j]==1)
+                {
+                    vector<pair<int,int>>vec;
+                    bfs(i,j,vis,vec,grid,i,j,n,m);
+                    s.insert(vec);
+                }
+            }
         }
-      }
-    }
-  public:
-    int countDistinctIslands(vector < vector < int >> & grid) {
-      int n = grid.size();
-      int m = grid[0].size();
-      vector < vector < int >> vis(n, vector < int > (m, 0));
-      set < vector < pair < int, int >>> st;
-
-      // traverse the grid
-      for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
-          // if not visited and is a land cell
-          if (!vis[i][j] && grid[i][j] == 1) {
-            vector < pair < int, int >> vec;
-            dfs(i, j, vis, grid, vec, i, j);
-            // store in set
-            st.insert(vec);
-          }
-        }
-      }
-      return st.size();
+        
+        return s.size();
     }
 };
 
