ae5c051605ed76effabbb845e68774c290c14c2e
Anupam Maurya <anupammaurya6767@gmail.com>
2023-07-16 15:03:04 +0530

    Added solution - LeetHub

--- Alien Dictionary - GFG/alien-dictionary.cpp
+++ Alien Dictionary - GFG/alien-dictionary.cpp
@@ -8,60 +8,67 @@ using namespace std;
 // User function Template for C++
 
 class Solution{
-    private:
-	vector<int> topoSort(int V, vector<int> adj[])
-	{
-		int indegree[V] = {0};
-		for (int i = 0; i < V; i++) {
-			for (auto it : adj[i]) {
-				indegree[it]++;
-			}
-		}
-
-		queue<int> q;
-		for (int i = 0; i < V; i++) {
-			if (indegree[i] == 0) {
-				q.push(i);
-			}
-		}
-		vector<int> topo;
-		while (!q.empty()) {
-			int node = q.front();
-			q.pop();
-			topo.push_back(node);
-			// node is in your topo sort
-			// so please remove it from the indegree
-
-			for (auto it : adj[node]) {
-				indegree[it]--;
-				if (indegree[it] == 0) q.push(it);
-			}
-		}
-
-		return topo;
-	}
-public:
-	string findOrder(string dict[], int N, int K) {
-		vector<int>adj[K];
-		for (int i = 0; i < N - 1; i++) {
-			string s1 = dict[i];
-			string s2 = dict[i + 1];
-			int len = min(s1.size(), s2.size());
-			for (int ptr = 0; ptr < len; ptr++) {
-				if (s1[ptr] != s2[ptr]) {
-					adj[s1[ptr] - 'a'].push_back(s2[ptr] - 'a');
-					break;
-				}
-			}
-		}
-
-		vector<int> topo = topoSort(K, adj);
-		string ans = "";
-		for (auto it : topo) {
-			ans = ans + char(it + 'a');
-		}
-		return ans;
-	}
+    public:
+    vector<int>solve(vector<int>adj[],int V)
+    {
+        vector<int>indegree(V,0);
+        for(int i=0;i<V;i++)
+        {
+            for(auto it:adj[i])
+            indegree[it]++;
+        }
+        
+        queue<int>q;
+        for(int i=0;i<V;i++)
+        if(indegree[i]==0)
+        q.push(i);
+        vector<int>ans;
+        
+        while(!q.empty())
+        {
+            int node = q.front();
+            q.pop();
+            ans.push_back(node);
+            for(auto it:adj[node])
+            {
+                indegree[it]--;
+                if(indegree[it]==0)
+                q.push(it);
+            }
+            
+        }
+        
+        return ans;
+    }
+    string findOrder(string dict[], int N, int K) {
+        //code here
+        vector<int>adj[K];
+        for(int i=0;i<N-1;i++)
+        {
+            int len=min(dict[i].size(),dict[i+1].size());
+            for(int it=0;it<len;it++)
+            {
+                if(dict[i][it]!=dict[i+1][it])
+                {
+                    adj[dict[i][it]-'a'].push_back(dict[i+1][it]-'a');
+                    break;
+                }
+            }
+        }
+        
+        
+        vector<int>topo = solve(adj,K);
+        
+        string ans = "";
+        for(auto it:topo)
+        {
+            ans+=char(it+'a');
+        }
+        
+        
+        return ans;
+        
+    }
 };
 
 //{ Driver Code Starts.
