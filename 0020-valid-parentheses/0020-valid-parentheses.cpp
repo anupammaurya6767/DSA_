ae79d33502759ac5b8e9a7dd9685302b1750d852
Anupam Maurya <anupammaurya6767@gmail.com>
2023-07-27 12:49:23 +0530

    Time: 4 ms (40.41%), Space: 6.3 MB (76.51%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -1,16 +1,50 @@
 class Solution {
 public:
     bool isValid(string s) {
-       stack<char> st; 
-        for(auto i:s)  
+        stack<int>st;
+        for(auto it:s)
         {
-            if(i=='(' or i=='{' or i=='[') st.push(i);  
-            else  
+            if(it=='(' || it=='{'|| it=='[')
+                st.push(it);
+            else if(it==')')
             {
-                if(st.empty() or (st.top()=='(' and i!=')') or (st.top()=='{' and i!='}') or (st.top()=='[' and i!=']')) return false;
-                st.pop(); 
+                if(st.empty())
+                    return false;
+                else
+                {
+                    if(st.top()=='(')
+                        st.pop();
+                    else
+                        return false;
+                }
+            }
+               
+            else if(it==']')
+            {
+                if(st.empty())
+                    return false;
+                else
+                {
+                    if(st.top()=='[')
+                        st.pop();
+                    else
+                        return false;
+                }
+            }
+            else if(it=='}')
+            {
+                if(st.empty())
+                    return false;
+                else
+                {
+                    if(st.top()=='{')
+                        st.pop();
+                    else
+                        return false;
+                }
             }
         }
-        return st.empty(); 
+        
+        return st.size()==0;
     }
 };
