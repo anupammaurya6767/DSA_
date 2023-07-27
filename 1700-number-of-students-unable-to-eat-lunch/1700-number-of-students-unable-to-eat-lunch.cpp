e7742b31e27383cb25f38fa3dc6c41336a230cb8
Anupam Maurya <anupammaurya6767@gmail.com>
2023-07-27 12:33:26 +0530

    Time: 0 ms (100.00%), Space: 8.6 MB (97.76%) - LeetHub

--- 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.cpp
+++ 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.cpp
@@ -9,8 +9,8 @@ public:
 	for(int i=0; i<sandwiches.size(); i++){
 		if(sandwiches[i] == 0){
 			if(circular_stu > 0){
-				student_count--;  // because he'll take it and leave the line.
-				circular_stu--;    // circular student is also gone.
+				student_count--;  
+				circular_stu--;   
 			}
 			else{return student_count;}
 		}
