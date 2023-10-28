b10409d9598e5fde0e8fe58e912907a6ba1ae116
Anupam Maurya <anupammaurya6767@gmail.com>
2023-10-28 21:15:08 +0530

    Create pattern18.cpp

---
+++ Patterns/pattern18.cpp
@@ -0,0 +1,28 @@
+void alphaTriangle(int n) {  
+
+   // Write your code here.      
+
+  
+
+      for(int i=1 ; i<=n ; i++){     
+
+ 
+
+           char k = 'A' + (n-1);             
+
+ 
+
+             for(int j=1 ; j<=i ; j++){         
+
+                   cout<<k<<" ";    
+
+                    k--;    
+
+                }   
+
+              cout<<endl;   
+
+       } 
+
+}
+
