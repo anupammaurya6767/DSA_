b41373b9bac0c25658cde4b12523b89422f75c7c
Anupam Maurya <anupammaurya6767@gmail.com>
2023-10-28 20:56:52 +0530

    Create pattern7.cpp

---
+++ Patterns/pattern7.cpp
@@ -0,0 +1,1 @@
+void nStarTriangle(int n) {    for(int i=1;i<=n;i++){        int space=n-i;        while(space>0){            cout<<" ";            space--;        }        for(int j=1;j<=i;j++){            cout<<"*";        }        for(int k=1;k<i;k++){            cout<<"*";        }    cout<<endl;    } }
