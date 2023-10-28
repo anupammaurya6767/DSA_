4268abd0ea2ef86f2efdb9e86e8a610c04647f1c
Anupam Maurya <anupammaurya6767@gmail.com>
2023-10-28 21:10:21 +0530

    Create pattern15.cpp

---
+++ Patterns/pattern15.cpp
@@ -0,0 +1,15 @@
+void nLetterTriangle(int n) {
+
+     for(int i=0;i<n;i++){
+
+        char ch= 'A';
+
+        for(int j=0; j<=n-i-1;j++){
+
+            cout<<ch++<<" ";
+
+           }
+
+        cout<<endl;
+
+    }}
